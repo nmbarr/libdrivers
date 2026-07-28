18abb5b7720a2dea16cce788fda2f12c07867a4b
Nicholas Barr <nickbarr10@gmail.com>
2026-07-28 14:24:37 -0700

    Initial commit of new lsm6dsl component driver

---
+++ include/libdrivers/lsm6dsl.h
@@ -0,0 +1,9 @@
+#ifndef LSM6DSL_H
+#define LSM6DSL_H
+
+#include "stm32l4xx_hal.h"
+#include "stm32l4xx_hal_def.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+#endif // LSM6DSL_H
---
+++ src/lsm6dsl.c
@@ -0,0 +1,2 @@
+#include "lsm6dsl.h"
+#include <stdint.h>
